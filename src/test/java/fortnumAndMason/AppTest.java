e4d908c216e765432128a4ebd8f4921cd6cbd42a
jaguar-ks <fahdamineksouss@gmail.com>
2026-03-30 15:29:21 +0100

    [UPDATE] : still working on bypassing the cookies pop up and getting the alert text

--- src/test/java/fortnumAndMason/AppTest.java
+++ src/test/java/fortnumAndMason/AppTest.java
@@ -2,18 +2,59 @@ package fortnumAndMason;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import fortnumAndMason.LoginPage;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.logging.LoggingHandler;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    private static WebDriver chromeDriver;
+    private LoginPage login;
 
-    /**
-     * Rigorous Test :-)
-     */
+    @BeforeAll
+    static void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        chromeDriver = new ChromeDriver(options);
+    }
+    
+    @AfterAll
+    static void cleanUp() {
+        if (chromeDriver != null)
+            chromeDriver.quit();
+    }
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void signIn() {
+        // try {
+        chromeDriver.get("https://www.fortnumandmason.com/login");
+        login = new LoginPage(chromeDriver);
+        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
+        // wait.until(login.isCookiePopUpActiv());
+        // login.rejectCookies();
+        wait.until(login.isPopUPActive());
+        login.closePopUp();
+        wait.until(login.isPageReady());
+        login.setEmail();
+        login.pressLoginButton();
+        wait.until(login.isPassWordReady());
+        login.setPassWord();
+        login.pressSignInButton();
+        assertTrue(login.isLogedIn(chromeDriver));
+        // } catch (Exception e) {
+        //     System.out.println("[Error] :: " + e);
+        // }
     }
 }
---
+++ src/test/java/fortnumAndMason/LoginPage.java
@@ -0,0 +1,90 @@
+package fortnumAndMason;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class LoginPage {
+    
+    @FindBy(css="input[aria-label='Email Address']")
+    private WebElement email;
+
+    @FindBy(css="input[name='password']")
+    private WebElement passWord;
+    
+    @FindBy(css="button[aria-label='Login'][type='submit']")
+    private WebElement loginButton;
+
+    @FindBy(css="button[aria-label='Close']")
+    private WebElement closePopUpButton;
+
+    @FindBy(css="button[data-testid='uc-deny-all-button']")
+    private WebElement rejectAllCookies;
+
+    @FindBy(css="button[aria-label='Sign-in'][type='submit']")
+    private WebElement signInButton;
+
+    private final Dotenv dotenv;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        dotenv = Dotenv.load();
+    }
+
+    public void setEmail() {
+        email.sendKeys(dotenv.get("EMAIL"));
+    }
+
+    public void setPassWord() {
+        passWord.sendKeys(dotenv.get("PASSWORD"));
+    }
+
+    public void pressLoginButton() {
+        loginButton.click();
+    }
+
+    public void pressSignInButton() {
+        signInButton.click();
+    }
+
+    public ExpectedCondition<WebElement> isPassWordReady() {
+        return ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector("input[name='password']")
+        );
+    }
+
+    public ExpectedCondition<WebElement> isCookiePopUpActiv() {
+        return ExpectedConditions.visibilityOfElementLocated(
+            By.id("uc-center-container")
+        );
+    }
+
+    public ExpectedCondition<WebElement> isPageReady() {
+        return ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector("input[aria-label='Email Address']")
+        );
+    }
+
+    public ExpectedCondition<WebElement> isPopUPActive() {
+        return ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector("button[aria-label='Close']")
+        );
+    }
+
+    public void closePopUp() {
+        closePopUpButton.click();
+    }
+
+    public void rejectCookies() {
+        rejectAllCookies.click();
+    }
+
+    public boolean isLogedIn(WebDriver driver) {
+        String text = driver.switchTo().alert().getText();
+        System.out.println("=> " + text);
+        return text.equals("Hello");
+    }
+}
